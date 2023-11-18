e405813689307275bb0804f7ca0e5a0c620bf3e3
jaderson <jader375@gmail.com>
2023-11-18 10:57:25 -0300

    update dishes

--- src/database/knex/migrations/20231118130223_dishes.js
+++ src/database/knex/migrations/20231118130223_dishes.js
@@ -1,15 +1,17 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
+exports.up = (knex) =>
+  knex.schema.createTable("dishes", (table) => {
+    table.uuid("id")
+    table.text("name").notNullable();
+    table.text("description").notNullable();
+    table.text("category").notNullable();
+    table.decimal("price", 10, 2).notNullable();
+    table.text("image").default(null);
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+    table.uuid("user_id").references("id").inTable("users");
+
+
+    table.timestamp("created_at").default(knex.fn.now());
+    table.timestamp("updated_at").default(knex.fn.now());
+  });
+
+exports.down = (knex) => knex.schema.dropTable("users");
