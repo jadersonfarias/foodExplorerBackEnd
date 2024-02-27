5d4eb2496efd39c7e6f94e0cd6aca448fa118c07
jaderson <jader375@gmail.com>
2024-02-27 14:36:30 -0300

    update doteenv

--- src/server.js
+++ src/server.js
@@ -1,35 +1,39 @@
 require("express-async-errors"); //sempre no começo
+require("dotenv/config");
+
 const express = require("express");
 
-const uploadConfig = require("./configs/upload")
-const AppError = require("./utils/AppError")
-const routes = require("./routes")
-const cors = require("cors")
+const uploadConfig = require("./configs/upload");
+const AppError = require("./utils/AppError");
+const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 
+app.use((error, request, response, next) => {
+  //serve para habilitar o tratamento de errors
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      // lado do cliente
+      status: "error",
+      message: error.message,
+    });
+  }
 
-app.use(( error, request, response, next ) => { //serve para habilitar o tratamento de errors
-        if(error instanceof AppError) {
-            return response.status(error.statusCode).json({ // lado do cliente
-                status: "error",
-                message: error.message
-            });
-        }
+  console.error(error); //caso for preciso fazer debug
 
-        console.error(error); //caso for preciso fazer debug
+  return response.status(500).json({
+    //lado do servidor
+    status: "error",
+    message: "internal server error",
+  });
+});
 
-        return response.status(500).json({ //lado do servidor
-            status: "error",
-            message: "internal server error",
-        });
-})
-
-const PORT = 3333;
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
